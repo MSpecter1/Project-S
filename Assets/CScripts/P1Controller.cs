a49a9cd71f6191ca7e960c1cd62ad4d72270c872
MSpecter1 <56405137+MSpecter1@users.noreply.github.com>
2019-10-31 02:17:11 -0700

    UI and Test Sprite
    Added Title Screen, Training Stage, Test Sprite, PlayerController, PauseMenu Controller, PauseScript,

---
+++ Assets/CScripts/P1Controller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class P1Controller : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool facingRight=false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (facingRight==false&&moveInput>0)
+        {
+            flip();
+        }
+        else if (facingRight==true&&moveInput>0)
+        {
+            flip();
+        }
+
+        
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector2(jumpForce, rb.velocity.x);
+        }
+        
+
+
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scalar = transform.localScale;
+        scalar.x*= -1;
+        transform.localScale=scalar;
+    }
+
+
+}
