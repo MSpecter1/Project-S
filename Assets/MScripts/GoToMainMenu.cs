d1c3805f1b28556264f8963360fa6f8a07deebf7
SebastianG7 <56668485+SebastianG7@users.noreply.github.com>
2019-11-11 05:40:10 -0800

    Basic Main Menu

--- Assets/MScripts/GoToMainMenu.cs
+++ Assets/MScripts/GoToMainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+
 public class GoToMainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
